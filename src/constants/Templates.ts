b9d4f33ad05628f750b11c51c639a8f04d3e6988
Bhaskar Sharma <bhaskarneeraj23@gamil.com>
2024-09-23 01:35:36 +0530

    fix#6

--- src/constants/Templates.ts
+++ src/constants/Templates.ts
@@ -8,6 +8,7 @@ export const defaultTemplate: TemplateStyle = {
   },
   skillFlexDirection: "column",
 };
+
 // section gray bg
 export const template2: TemplateStyle = {
   personalDetail: {
@@ -20,9 +21,61 @@ export const template2: TemplateStyle = {
   skillFlexDirection: "row",
 };
 
+// section blue bg with left-aligned text
+export const template3: TemplateStyle = {
+  personalDetail: {
+    align: "flex-start",
+  },
+  sectionHeading: {
+    bg: "lightblue",
+    text: "darkblue",
+  },
+  skillFlexDirection: "row",
+};
+
+// section dark bg with white text and center-aligned text
+export const template4: TemplateStyle = {
+  personalDetail: {
+    align: "center",
+  },
+  sectionHeading: {
+    bg: "black",
+    text: "white",
+  },
+  skillFlexDirection: "column",
+};
+
+// section light bg with bold text
+export const template5: TemplateStyle = {
+  personalDetail: {
+    align: "flex-end",
+  },
+  sectionHeading: {
+    bg: "lightgray",
+    text: "darkgray",
+  },
+  skillFlexDirection: "row",
+};
+
+// section dark bg with pink text and center-aligned text
+export const template6: TemplateStyle = {
+  personalDetail: {
+    align: "center",
+  },
+  sectionHeading: {
+    bg: "#f00b51",
+    text: "white",
+  },
+  skillFlexDirection: "row",
+};
+
 const templateList: TemplateList[] = [
   { id: "1", template: defaultTemplate },
   { id: "2", template: template2 },
+  { id: "3", template: template3 },
+  { id: "4", template: template4 },
+  { id: "5", template: template5 },
+  { id: "6", template: template6 },
 ];
 
 export const templatePreviewList: { id: string; templateImage: string }[] = [
@@ -34,5 +87,22 @@ export const templatePreviewList: { id: string; templateImage: string }[] = [
     id: "2",
     templateImage: "/template/template2.png",
   },
+  {
+    id: "3",
+    templateImage: "/template/template3.png",
+  },
+  {
+    id: "4",
+    templateImage: "/template/template4.png",
+  },
+  {
+    id: "5",
+    templateImage: "/template/template5.png",
+  },
+  {
+    id: "6",
+    templateImage: "/template/template6.png",
+  },
 ];
+
 export default templateList;
--- src/pages/ChooseTemplate.tsx
+++ src/pages/ChooseTemplate.tsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 
 const ChooseTemplate = () => {
   return (
-    <div className="w-full h-svh  py-8 bg-black flex flex-col item-center overflow-y-auto">
+    <div className="w-full h-svh  py-8 bg-black flex flex-col item-center overflow-y-auto text-white">
       <h1 className=" text-pink-700 font-bold text-3xl sm:text-6xl mb-5 sm:mb-20 text-center">
         Choose Template
       </h1>
-      <div className="flex items-center justify-start flex-wrap w-fit gap-x-[100px] gap-y-[50px] max-w-[1200px] mx-auto ">
+      <div className="flex justify-center items-center sm:justify-start flex-wrap w-full gap-[50px] max-w-[90%] sm:max-w-[80%]  mx-auto ">
         {templateList.map(({ id }, index) => (
           <Link
             to={`/resume/${id}`}
             key={id}
-            className="w-[180px] sm:w-[213px] h-[250px] sm:h-[300px] flex-grow-0 flex-shrink-0  pdf-container  relative mx-auto rounded-lg overflow-hidden  hover:scale-[1.04] transition-all cursor-pointer m-0 hide-scrollbar shadow shadow-white flex justify-center items-center border-2 border-neutral-900  p-0.5"
+            className="w-[180px] sm:w-[213px] h-[250px] sm:h-[300px] flex-grow-0 flex-shrink-0  relative rounded-lg overflow-hidden  hover:scale-[1.04] transition-all cursor-pointer m-0 hide-scrollbar shadow shadow-white flex justify-center items-center border-2 border-neutral-900  p-0.5 bg-neutral-700"
           >
             <img
               src={templatePreviewList[index].templateImage}
